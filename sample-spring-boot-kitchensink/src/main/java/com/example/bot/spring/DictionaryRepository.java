85daa7f5784c11ca22d22edb5119335880419feb
pgabriela <pgabriela@connect.ust.hk>
2017-10-10 02:01:23 +0800

    DB TEST #30

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/DictionaryRepository.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/DictionaryRepository.java
@@ -1,6 +1,7 @@
 package com.example.bot.spring;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.Repository;
 import org.springframework.stereotype.Component;
 import java.util.List;
 
